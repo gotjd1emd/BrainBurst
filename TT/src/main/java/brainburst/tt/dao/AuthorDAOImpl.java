69e29dd173f19689a2f3e6bdc022141ec2396f52
bigshoota <unb_holic@naver.com>
2016-12-07 17:05:55 +0900

    annotaion
    ...

--- TT/src/main/java/brainburst/tt/dao/AuthorDAOImpl.java
+++ TT/src/main/java/brainburst/tt/dao/AuthorDAOImpl.java
@@ -2,13 +2,18 @@ package brainburst.tt.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
 import brainburst.tt.dto.WebtoonDTO;
 
+@Repository
 public class AuthorDAOImpl implements AuthorDAO {
-
+	private SqlSession sqlSession;
+	
 	@Override
 	public List<WebtoonDTO> getMyWebtoon(String nickname) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 }
--- TT/src/main/java/brainburst/tt/service/AuthorServiceImpl.java
+++ TT/src/main/java/brainburst/tt/service/AuthorServiceImpl.java
@@ -2,8 +2,11 @@ package brainburst.tt.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import brainburst.tt.dto.WebtoonDTO;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
 	/**
 	 * 내 웹툰 상세보기
