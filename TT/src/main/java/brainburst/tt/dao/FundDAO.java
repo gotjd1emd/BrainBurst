23fae966c4cda69b620fba20c09c0687a45599a4
bigshoota <unb_holic@naver.com>
2016-12-08 18:52:50 +0900

    fund add..
    add..

---
+++ TT/src/main/java/brainburst/tt/dao/FundDAO.java
@@ -0,0 +1,14 @@
+package brainburst.tt.dao;
+
+import java.util.List;
+
+import brainburst.tt.dto.WebtoonDTO;
+
+public interface FundDAO {
+	/**
+	 * 사용자의 펀딩중인 웹툰 목록로딩
+	 * @param nickname 사용자의 이름
+	 * @return 해당웹툰 리스트
+	 */
+	List<WebtoonDTO> fundPage(String nickname);
+}
