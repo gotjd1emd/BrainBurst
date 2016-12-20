e1f6d1f9207a82a17430e22e60de076395fbb18c
ImHaeSung <gotjd1emd@gmail.com>
2016-12-20 15:07:46 +0900

    add log4j
    add log4j

---
+++ TT/src/main/java/brainburst/tt/timeTask/EpisodeUploadDeadline.java
@@ -0,0 +1,13 @@
+package brainburst.tt.timeTask;
+
+import java.util.TimerTask;
+
+public class EpisodeUploadDeadline extends TimerTask {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("EpisodeUploadDeadline");
+	}
+
+}
